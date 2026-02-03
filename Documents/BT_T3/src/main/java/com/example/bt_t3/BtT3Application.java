6ab25f65ce127d6a5d2db75ea4adf2266b1aca72
Nguyen Quang Thanh <q.thanh10022004@gmail.com>
2026-02-03 16:14:43 +0700

    BT_T3

---
+++ Documents/BT_T3/src/main/java/com/example/bt_t3/BtT3Application.java
@@ -0,0 +1,13 @@
+package com.example.bt_t3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtT3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BtT3Application.class, args);
+    }
+
+}
---
+++ Documents/BT_T3/src/test/java/com/example/bt_t3/BtT3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bt_t3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtT3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
